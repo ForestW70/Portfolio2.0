18d5e1325ee9a045071961ccf116765533a01978
ForestW70 <hexaforest@gmail.com>
2021-04-16 20:18:55 -0400

    ehh lets try again

--- README.md
+++ README.md
@@ -11,3 +11,30 @@ weewoo
 ## Screenshots
 
 ## Relevant links
+
+
+--APIs--
+
+JQuery
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+
+Bootstrap
+<link 
+  rel="stylesheet" 
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+  integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"></link>
+
+<script 
+  src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+Momentjs
+<link   
+  src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></link>
+
+
+
+
+
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,38 @@
-console.log("welcome to scriptville");
+
+const projSquares = document.querySelectorAll(".title-cont");
+const bigDisp = document.getElementById("showCase");
+const topicButtons = document.querySelectorAll(".title-cont, .off, .hover-on")
+
+
+projSquares.forEach( (elem) => {
+
+
+    elem.addEventListener("mouseenter", e => {
+        e.target.firstElementChild.classList.add("hide");
+        e.target.lastElementChild.classList.remove("hide");
+    })
+
+    elem.addEventListener("mouseleave", e => {
+        e.target.firstElementChild.classList.remove("hide");
+        e.target.lastElementChild.classList.add("hide");
+    })
+
+    elem.lastElementChild.addEventListener("click", e => {
+        let selecTopic = e.target.getAttribute("name");
+        console.log(e.target)
+        console.log(selecTopic);
+        displayBigBox(selecTopic)
+    })
+    
+
+    
+})
+
+
+function displayBigBox(topicName) {
+    // el.preventDefault;
+    // bigDisp.classList.add("new-pic");
+    // let contentPic = document.getElementsByName(`"${topicName}"`)
+    bigDisp.style.backgroundImage = `url(./assets/images/${topicName}.png)`;
+
+}
